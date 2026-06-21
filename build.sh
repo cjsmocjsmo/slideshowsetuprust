afd9415aa7f1099c7f34a2c1f181d1f808229e9e
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-06-21 00:45:23 -0700

    feat: update build script to use correct binary names and improve copy commands

--- build.sh
+++ build.sh
@@ -6,29 +6,20 @@ else
     exit 1
 fi
 
-# ARM64=./target/release/rusicsetup
-# ARM32=./target/armv7-unknown-linux-gnueabihf/release/rusicsetup
-# ENV=./.env
+ARM64=./target/release/slideshowsetuprust
+ARM32=./target/armv7-unknown-linux-gnueabihf/release/slideshowsetuprust
 
-# if [ -f "$ARM64" ]; then
-#     echo "Copying ARM64 binary to current directory..."
-#     cp "$ARM64" ./rusicsetup-rpi4-"$1"
-#     cp "$ARM64" ../rusic/setup/rusicsetup-rpi4-"$1"
-# else
-#     echo "ARM64 binary not found. Please ensure it was built successfully."
-# fi
+if [ -f "$ARM64" ]; then
+    echo "Copying ARM64 binary to current directory..."
+    cp "$ARM64" ./slideshowsetuprust-rpi4-"$1"
+else
+    echo "ARM64 binary not found. Please ensure it was built successfully."
+fi
 
-# if [ -f "$ARM32" ]; then
-#     echo "Copying ARM32 binary to current directory..."
-#     cp "$ARM32" ./rusicsetup-rpi3b-"$1"
-#     cp "$ARM32" ../rusic/setup/rusicsetup-rpi3b-"$1"
-# else
-#     echo "ARM32 binary not found. Please ensure it was built successfully."
-# fi
+if [ -f "$ARM32" ]; then
+    echo "Copying ARM32 binary to current directory..."
+    cp "$ARM32" ./slideshowsetuprust-rpi3b-"$1"
+else
+    echo "ARM32 binary not found. Please ensure it was built successfully."
+fi
 
-# if [ -f "$ENV" ]; then
-#     echo "Copying .env file to setup directory..."
-#     cp "$ENV" ../rusic/setup/.env
-# else
-#     echo ".env file not found. Please ensure it exists in the root of your project."
-# fi
